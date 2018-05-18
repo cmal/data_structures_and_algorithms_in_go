571f34e3e31b55c7d7e58cc4c3622ba8d851b6ad
Yu Zhao <yu.zhao@joudou.com>
2018-05-18 22:12:11 +0800

    finished infix to postfix

--- ch08/infix_to_postfix.go
+++ ch08/infix_to_postfix.go
@@ -3,59 +3,75 @@ package main
 import (
 	"fmt"
 	"github.com/golang-collections/collections/stack"
+	"github.com/golang-collections/collections/queue"
 )
 
 func main() {
-
+	fmt.Println(InfixToPostfix("1+2*3"))
+	fmt.Println(InfixToPostfix("(1+2)*3"))
+	fmt.Println(InfixToPostfix("(1+2)*(3+4)"))
+	fmt.Println(InfixToPostfix("(1*2)+(3*4)"))
+	fmt.Println(InfixToPostfix("1^2*3+4"))
+	fmt.Println(InfixToPostfix("1+2*3^4"))
 }
 
 func InfixToPostfix(expn string) string {
-	fmt.Println(expn)
-	stk := new(Stack)
-	resStk := new(Stack)
+	fmt.Printf("%v    =>    ", expn)
+	stk := stack.New()
+	resQ := queue.New()
 	output := ""
 
 	for _, ch := range expn {
 		if ch >= '0' && ch <= '9' {
-			stk.Push(ch)
+			// fmt.Println("Enqueue " + string(ch))
+			resQ.Enqueue(ch)
 		} else {
 			switch ch {
 			case '+', '-', '*', '/', '%', '^':
-				for !stk.IsEmpty() {
-					head := stk.Pop()
-					if precedence(ch) >= precedence(head) && head != '(' {
-						resStk.Push(head)
-					} else {
-						stk.Push(head)
+				// fmt.Println("stack len: ", stk.Len())
+				if stk.Len() != 0 {
+					peek := stk.Peek().(rune)
+					// fmt.Println("Compare ", string(ch), " to ", string(peek))
+					if precedence(ch) <= precedence(peek) {
+						for stk.Len() != 0 {
+							// fmt.Println("Pop")
+							head := stk.Pop().(rune)
+							if head == '(' {
+								break
+							}
+							// fmt.Println("Enqueue " + string(head))
+							resQ.Enqueue(head)
+						}
 					}
 				}
-				stk.push(ch)
+				// fmt.Println("Push " + string(ch))
+				stk.Push(ch)
 			case '(':
+				// fmt.Println("Push " + string(ch))
 				stk.Push(ch)
 			case ')':
-				for !stk.IsEmpty() {
-					head := stk.Pop()
+				for stk.Len() != 0 {
+					// fmt.Println("Pop")
+					head := stk.Pop().(rune)
 					if head == '(' {
-						break;
-					} else {
-						resStk.Push(head)
+						break
 					}
+					// fmt.Println("Enqueue " + string(head))
+					resQ.Enqueue(head)
 				}
 			}
 		}
-		for stk.IsEmpty() == false {
-			head := stk.Pop()
-			resStk.Push(head)
-		}
+	}
+	for stk.Len() != 0 {
+		// fmt.Println("Pop ")
+		head := stk.Pop().(rune)
+		// fmt.Println("Enqueue " + string(head))
+		resQ.Enqueue(head)
 	}
 
-	s := make([]rune, resStk.Len())
-	i := 0
-	for resStk.IsEmpty() {
-		s[i++] = resStk.Pop().(rune)
-	}
-	for _, item = range s {
-		output += item + " "
+	for resQ.Len() != 0 {
+		head := resQ.Dequeue().(rune)
+		output += string(head) + " "
 	}
 	return output
 }
